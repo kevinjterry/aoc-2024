b7eba315ed9419d74d06e2903971639259aa8d50
Kevin Terry <kevinjterry@gmail.com>
2024-12-02 18:35:54 -0500

    Refactor data loading and safety checks; improve readability and efficiency

--- Day 2/main.py
+++ Day 2/main.py
@@ -3,44 +3,30 @@ def load_data(filename):
     with open(filename, "r") as f:
         lines = [line.strip() for line in f.readlines()]
 
-    not_safe_list = []
-    safe_list = []
-
-    for line in lines:
-        is_safe = test_if_safe(line)
-        if is_safe:
-            # print(f"Line {line} YES")
-            safe_list.append(line)
-        else:
-            print(f"Line {line} NO")
-            not_safe_list.append(line)
+    safe_list = [line for line in lines if test_if_safe(line)]
+    unsafe_list = [line for line in lines if not test_if_safe(line)]
 
     print(f"Safe: {len(safe_list)}")
-    print(f"Not Safe: {len(not_safe_list)}")
+    print(f"Unsafe: {len(unsafe_list)}")
 
-    return safe_list, not_safe_list
+    return safe_list, unsafe_list
 
 
 def test_if_safe(line):
-    line = line.split(" ")
+    numbers = [int(x) for x in line.split()]
+    ascending = sorted(numbers)
+    descending = sorted(numbers, reverse=True)
 
-    # check if there are any duplicates
-    if len(line) != len(set(line)):
-        # print(f"Line {line} has duplicates")
-        return False
-
-    # check if ascending or descending
-    if line == sorted(line) or line == sorted(line, reverse=True):
-
-        # if any numbers have a difference of more than 2
-        for i in range(1, len(line)):
-            if abs(int(line[i]) - int(line[i - 1])) > 4:
-                print(f"Line {line} has a difference of more than 4")
+    if numbers == ascending or numbers == descending:
+        for i in range(1, len(numbers)):
+            if abs(numbers[i] - numbers[i - 1]) > 3:
                 return False
+
+        if len(numbers) != len(set(numbers)):
+            return False
 
         return True
 
-    # print(f"Line {line} is not in order")
     return False
 
 
@@ -49,4 +35,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    raise SystemExit(main())
